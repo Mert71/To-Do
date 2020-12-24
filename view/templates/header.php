50acb4b2db7c030edbe29bae2dca07a98e0c42e0
Mert71 <99048479@mydavinci.nl>
2020-12-24 20:49:23 +0100

    Created view Task files and added Css

--- view/templates/header.php
+++ view/templates/header.php
@@ -3,13 +3,15 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Studenten app</title>
-	<link rel="stylesheet" href="<?= URL ?>">
+	<title>To-do list</title>
+	<link rel="stylesheet" href="<?= URL ?>public/css/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
-	<nav>
-	<ul>
+	<nav class="bg-primary">
+	<ul class="nav nav-pills">
 		<li class="nav-item"><a class="nav-link" href="<?= URL ?>home/index">Home</a></li>
-		<li class="nav-item"><a class="nav-link" href="<?= URL ?>song/index">Lijsten</a></li>	
+		<li class="nav-item"><a class="nav-link" href="<?= URL ?>song/index">Lijsten</a></li>
 	</ul>
 	</nav>
